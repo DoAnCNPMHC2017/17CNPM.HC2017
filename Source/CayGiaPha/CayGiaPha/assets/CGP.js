9927ba2a06f01ac221c0d7608e08437ed45798f1
VuongNguyen <dragonblue789@gmail.com>
2018-02-06 01:13:27 +0700

    fix lỗi xung đôt

--- Source/CayGiaPha/CayGiaPha/assets/CGP.js
+++ Source/CayGiaPha/CayGiaPha/assets/CGP.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function () {
     CreateControl();
     LoadData();
+    ControlCreateTree();
 });
 
 function CreateControl() {
@@ -64,38 +65,7 @@ function CreateControl() {
         dateInput: true,
         footer: "Hôm nay - #: kendo.toString(data, 'dd/MM/yyyy') #"
     });
-    //$('#GridListHDDT').kendoGrid({
-    //    //toolbar: ["excel"],
-    //    //excel: {
-    //    //    allPages: true,
-    //    //    fileName: "ListStreet.xlsx"
-    //    //},
-    //    resizable: true,
-    //    selectable: true,
-    //    scrollable: true,
-    //    height: 500,
-    //    pageable: {
-    //        pageSizes: true,
-    //        refresh: true,
-    //        buttonCount: 5,
-    //        pageSize: 100,
-    //        pageSizes: [100, 250, 500, 1000],
-    //        messages: {
-    //            itemsPerPage: "dòng / trang",
-    //            display: "Hiển thị {0} - {1} / {2}",
-    //            empty: "Không tìm thấy dữ liệu"
-    //        }
-    //    },
-    //    filterable: {
-    //        extra: false,
-    //        messages: { and: "và", or: "hoặc", filter: "Lọc", clear: "Hủy lọc", info: "" },
-    //        operators: {
-    //            string: { eq: "Bằng", neq: "Khác", startswith: "Bắt đầu từ", contains: "Chứa", doesnotcontain: "Không chứa", endswith: "Kết thúc bằng" }
-    //            , number: { eq: "=", neq: "!=", gte: ">=", gt: ">", lte: "<=", lt: "<" }
-    //            , date: { neq: "!=", gte: ">=", gt: ">", lte: "<=", lt: "<" }
-    //        }
-    //    },
-    //});
+    
     //$('#GridListDetailHDDT').kendoGrid({
     //    //toolbar: ["excel"],
     //    excel: {
@@ -142,5 +112,40 @@ function LoadData()
         },
         error: function () {
         }
+    });
+}
+function ControlCreateTree()
+{
+    $('#GridTree').kendoGrid({
+        //toolbar: ["excel"],
+        //excel: {
+        //    allPages: true,
+        //    fileName: "ListStreet.xlsx"
+        //},
+        resizable: true,
+        selectable: true,
+        scrollable: true,
+        height: 500,
+        pageable: {
+            pageSizes: true,
+            refresh: true,
+            buttonCount: 5,
+            pageSize: 100,
+            pageSizes: [100, 250, 500, 1000],
+            messages: {
+                itemsPerPage: "dòng / trang",
+                display: "Hiển thị {0} - {1} / {2}",
+                empty: "Không tìm thấy dữ liệu"
+            }
+        },
+        filterable: {
+            extra: false,
+            messages: { and: "và", or: "hoặc", filter: "Lọc", clear: "Hủy lọc", info: "" },
+            operators: {
+                string: { eq: "Bằng", neq: "Khác", startswith: "Bắt đầu từ", contains: "Chứa", doesnotcontain: "Không chứa", endswith: "Kết thúc bằng" }
+                , number: { eq: "=", neq: "!=", gte: ">=", gt: ">", lte: "<=", lt: "<" }
+                , date: { neq: "!=", gte: ">=", gt: ">", lte: "<=", lt: "<" }
+            }
+        },
     });
 }
