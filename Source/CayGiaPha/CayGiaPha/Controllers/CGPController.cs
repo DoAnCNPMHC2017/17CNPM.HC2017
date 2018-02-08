a1931492d8623dbd9873104a857edabf1cad88e0
1742013 <nnbduong@gmail.com>
2018-02-08 19:47:15 +0700

    Cập nhật ràng buộc
    ràng buộc đăng nhập

--- Source/CayGiaPha/CayGiaPha/Controllers/CGPController.cs
+++ Source/CayGiaPha/CayGiaPha/Controllers/CGPController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CayGiaPha.Helper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,13 +10,14 @@ namespace CayGiaPha.Controllers
     public class CGPController : Controller
     {
         // GET: CGP
-        
+       
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: CGP
+        // GET: CGP 
+        [CheckLogin]
         public ActionResult CreateCGP()
         {
             return View();
